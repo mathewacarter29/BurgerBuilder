605af5b87d95dd82c7a8312ca8761789be82aed4
mcarte01 <mcarte01@peraton.com>
2021-09-17 13:47:10 -0400

    Added UI enhacements and buttons to change ingredient amounts

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,19 +1,38 @@
-import React from 'react';
-import classes from './Burger.module.css'
-import BurgerIngredient from './BurgerIngredients/BurgerIngredient'
+import React from "react";
+import classes from "./Burger.module.css";
+import BurgerIngredient from "./BurgerIngredients/BurgerIngredient";
 
 //Holds all the ingredients in the burger
 const burger = (props) => {
-    return(
-        <div className={classes.Burger}>
-            <BurgerIngredient type="bread-top"></BurgerIngredient>
-            <BurgerIngredient type="cheese"></BurgerIngredient>
-            <BurgerIngredient type="salad"></BurgerIngredient>
-            <BurgerIngredient type="bacon"></BurgerIngredient>
-            <BurgerIngredient type="meat"></BurgerIngredient>
-            <BurgerIngredient type = "bread-bottom"></BurgerIngredient>
-        </div>
-    );
+  /*
+  1. turns ingredients object into array of ingredient names
+  2. turns each element of this ingredient name array into an empty array of length (ingredient #)
+  3. turns each element of the empty array into a jsx of the correct burger ingredient
+  */
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igName) => {
+      return [...Array(props.ingredients[igName])].map((_, i) => {
+        return (
+          <BurgerIngredient key={igName + i} type={igName}></BurgerIngredient>
+        );
+      });
+    })
+    .reduce((arr, el) => {
+      //concat() will merge 2 or more arrays into 1
+      return arr.concat(el);
+    }, []);
+  console.log(transformedIngredients);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients</p>;
+  }
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top"></BurgerIngredient>
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom"></BurgerIngredient>
+    </div>
+  );
 };
 
 export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,18 +1,96 @@
-import React, { Component } from 'react'
-import Aux from '../../hoc/Auxiliary'
-import Burger from '../../components/Burger/Burger'
+import React, { Component } from "react";
+import Aux from "../../hoc/Auxiliary";
+import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+import Modal from "../../components/UI/Modal/Modal";
+import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7,
+};
 
 //Holds the burger and the build controls
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0,
+    },
+    totalPrice: 4,
+    purchasable: false,
+  };
 
-    render() {
-        return (
-            <Aux>
-                <Burger></Burger>
-                <div>Build Controls</div>
-            </Aux>
-        );
+  updatePurchaseState(ingredients) {
+    const sum = Object.keys(ingredients)
+      .map((igName) => {
+        return ingredients[igName];
+      })
+      .reduce((total, el) => {
+        return total + el;
+      }, 0);
+    this.setState({ purchasable: sum > 0 });
+  }
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {
+      ...this.state.ingredients,
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+    this.updatePurchaseState(updatedIngredients);
+  };
+
+  removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    //Handles trying to remove an ingredient that is not there
+    if (oldCount <= 0) {
+      return;
     }
+    const updatedCount = oldCount - 1;
+    const updatedIngredients = {
+      ...this.state.ingredients,
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+    this.updatePurchaseState(updatedIngredients);
+  };
+
+  render() {
+    const disabledInfo = {
+      ...this.state.ingredients,
+    };
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
+    return (
+      <Aux>
+        <Modal>
+          <OrderSummary ingredients={this.state.ingredients}></OrderSummary>
+        </Modal>
+        <Burger ingredients={this.state.ingredients}></Burger>
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+          disabled={disabledInfo}
+          price={this.state.totalPrice}
+          purchasable={!this.state.purchasable}
+        ></BuildControls>
+      </Aux>
+    );
+  }
 }
 
 export default BurgerBuilder;
